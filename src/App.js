b08b1e3f933ded505c05ae56a2bddea75a4e9d0a
waelalsabbah <alsabbahwael@gmail.com>
2023-09-19 20:57:57 +0200

    Make a clone landing page

--- src/App.js
+++ src/App.js
@@ -4,37 +4,39 @@ export default function App() {
   return (
     <>
       <main>
-        Main
         <section>
-          <header>
-            <nav>
-              <a href="/#">logo</a>
-              <ul>
-                <li>
-                  <a href="/#">Inspirasion</a>
-                </li>
-                <li>
-                  <a href="/#">Component Library</a>
-                </li>
-                <li>
-                  <a href="/#">Templates</a>
-                </li>
-                <li>
-                  <a href="/#">Learn</a>
-                </li>
-                <li>
-                  <a href="/#">Tools</a>
-                </li>
-                <li>
-                  <a href="/#">Pricing</a>
-                </li>
-              </ul>
-              <a href="/#">Support</a>
-            </nav>
-          </header>
-          sectoin1
+          <div className={styles.container}>
+            <header>
+              <nav>
+                <a href="/#">logo</a>
+
+                <ul>
+                  <li>
+                    <a href="/#">Inspirasion</a>
+                  </li>
+                  <li>
+                    <a href="/#">Component Library</a>
+                  </li>
+                  <li>
+                    <a href="/#">Templates</a>
+                  </li>
+                  <li>
+                    <a href="/#">Learn</a>
+                  </li>
+                  <li>
+                    <a href="/#">Tools</a>
+                  </li>
+                  <li>
+                    <a href="/#">Pricing</a>
+                  </li>
+                </ul>
+                <a href="/#">Support</a>
+              </nav>
+            </header>
+            sectoin1
+          </div>
         </section>
-        <section>sectoin2</section>
+        <section>Canvas</section>
         <section>sectoin3</section>
       </main>
 
